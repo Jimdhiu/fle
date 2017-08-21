f46c149f8dd17175a179c94b5cd75a04f946cc83
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-21 18:59:12 +0200

    seed presque fonctionnel

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#créer les catégories
+#créer les tags
+#créer un ou 2 users
+#créer des documents
+Document.destroy_all
+Category.destroy_all
+Tag.destroy_all
+User.destroy_all
+
+
+categories = ["Pièces d'identité", "Auto",
+ "Finance", "Emploi", "Médical", "Habitation", "Éducation"]
+
+tags = ["Carte d'identité", "Passport", "Permis de conduire", "Numéro de sécurité social",
+  "Bulletin de paie", "Justificatif de domicile", "Avis d'imposition", "Relevé de compte bancaire"]
+
+categories.each do |category|
+  Category.create(name: category)
+end
+
+puts "Catégories créées .... "
+puts "-----------------------"
+
+
+
+tags.each do |tag|
+  Tag.create(name: tag)
+end
+
+puts "Tags crées ...."
+puts "---------------"
+
+user = User.create(email: "test@gmail.com", password: "password")
+
+puts "1 user créé !"
+
+
+Document.create(verified: true, document_date: Date.new(2017,3,8),
+  expiration_date: Date.new(2017,3,8) + 200, user: user,
+  category: Category.first,
+  tag: Tag.first)
+
+Document.create(verified: true, document_date: Date.new(2017,3,8),
+  expiration_date: Date.new(2017,3,8) + 200, user: user,
+  category: Category.,
+  tag: Tag.all.sample)
+
+Document.create(verified: true, document_date: Date.new(2017,3,8),
+  expiration_date: Date.new(2017,3,8) + 200, user: user,
+  category: Category.all.sample,
+  tag: Tag.all.sample)
+
+
+
