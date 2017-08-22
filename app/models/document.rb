ef417c6f8e665bd1ba178206ccfff05c1ba79fa6
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-22 16:47:24 +0200

    changement shema de DB

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,5 @@
 class Document < ApplicationRecord
   belongs_to :user
-  belongs_to :category
   belongs_to :tag
 
   has_attachment :photo
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
   has_many :documents
+  has_many :categories
 end
