cc9688e91a78c1dcb216452290e7e6ccf16d398c
af <a.fabiano@live.fr>
2017-08-23 15:11:14 +0200

    Validation Doc Ok

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,5 +2,9 @@ class Document < ApplicationRecord
   belongs_to :user
   belongs_to :tag
 
+  validates :photo, presence: true
+  validates :document_date, presence: true
+  validates :tag_id, presence: true
+
   has_attachment :photo
 end
