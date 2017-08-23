ea3309b88bb98625155dc116b82574beaa51d831
jorge ruivinho <jorge.ruivinho@me.com>
2017-08-23 15:06:04 +0200

    add validity and number of uploads to tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822145416) do
+ActiveRecord::Schema.define(version: 20170823125415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,9 +50,11 @@ ActiveRecord::Schema.define(version: 20170822145416) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.integer  "category_id"
+    t.integer  "validity",          default: 0, null: false
+    t.integer  "number_of_uploads", default: 0, null: false
     t.index ["category_id"], name: "index_tags_on_category_id", using: :btree
   end
 
