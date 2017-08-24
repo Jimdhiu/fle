d21cdf507eead2893c5da5790be8b085083a0baa
jimmy dhiu <dhiu.jim@gmail.com>
2017-08-24 16:44:19 +0200

    model et controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'documents#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :documents, only: [:new, :create, :show, :edit, :update, :destroy ]
-
+  resources :procedures, only: [:new, :create, :show, :edit, :update, :destroy ]
 end
