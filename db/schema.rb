7e2187c0b7e1c63b1b257087ddf7805d49b92398
jimmy dhiu <dhiu.jim@gmail.com>
2017-08-28 16:50:34 +0200

    add second kind of user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825134246) do
+ActiveRecord::Schema.define(version: 20170828105005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20170825134246) do
     t.index ["user_id"], name: "index_documents_on_user_id", using: :btree
   end
 
-
   create_table "procedure_documents", force: :cascade do |t|
     t.integer  "procedure_request_id"
     t.integer  "document_id"
@@ -72,7 +71,6 @@ ActiveRecord::Schema.define(version: 20170825134246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "requested_tags", force: :cascade do |t|
     t.integer  "procedure_id"
@@ -112,6 +110,7 @@ ActiveRecord::Schema.define(version: 20170825134246) do
     t.string   "compagny"
     t.string   "function"
     t.string   "status"
+    t.integer  "role"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
