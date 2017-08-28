cdb819ee8b31043ec3b42e4a15f5695ed178bef8
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-28 12:11:21 +0200

    moitié des categories faites

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
+#créer 2 procédure : credit immo et
+#intégrer les tags requis par procédures
+#créer tous les tags
 #créer les catégories
 #créer les tags
-#créer un ou 2 users
+
+#créer un user
 #créer des documents
 
 
@@ -48,6 +52,7 @@ documents = {
     diplomas: "Diplomes"
   }
 }
+credit_immo = Procedure.create()
 
 personnel = Category.create(name:'Personnel', icon: 'fa fa-user')
 auto = Category.create(name: "Auto", icon: 'fa fa-car')
@@ -60,20 +65,34 @@ education = Category.create(name: "Education", icon: 'fa fa-graduation-cap')
 puts "7 catégories créées"
 puts "-------------------"
 
-t1 = Tag.create(name: "Carte d'identité", category: personnel, validity: 10*365, number_of_uploads: 1)
-t2 = Tag.create(name: "Passeport", category: personnel, validity: 10*365, number_of_uploads: 1)
-t3 = Tag.create(name: "Permis de conduire", category: auto, validity: 10*365, number_of_uploads: 1)
-t4 = Tag.create(name: "Carte grise", category: auto, validity: 10*365, number_of_uploads: 1)
-t5 = Tag.create(name: "RIB", category: finance, validity: 10*365, number_of_uploads: 1)
-t6 = Tag.create(name: "Relevés bancaire", category: finance, validity: 10, number_of_uploads: 1)
-t7 = Tag.create(name: "Bulletins de salaire", category: emploi, validity: 10*365, number_of_uploads: 4)
-t8 = Tag.create(name: "Contrat de travail", category: emploi, validity: 10*365, number_of_uploads: 1)
-t9 = Tag.create(name: "Carnet de santé", category: medical, validity: 10*365, number_of_uploads: 1)
-t10 = Tag.create(name: "Carte vitale", category: medical, validity: 5*365, number_of_uploads: 1)
-t11 = Tag.create(name: "Justificatif de domicile", category: habitation, validity: 60, number_of_uploads: 1)
-t12 = Tag.create(name: "Quittance de loyer", category: habitation, validity: 10*365, number_of_uploads: 1)
-t13 = Tag.create(name: "Certificat de scolarité", category: education, validity: 10*365, number_of_uploads: 1)
-t14 = Tag.create(name: "Diplomes", category: education, validity: 10*365, number_of_uploads: 1)
+identity_card = Tag.create(name: "Carte d'identité", category: personnel, validity: 10 * 365, number_of_uploads: 2)
+family_book = Tag.create(name: "Livret de famille", category: personnel, validity: 100 * 365, number_of_uploads: 2)
+identity_card = Tag.create(name: "Attestation de responsabilité civile", category: personnel, validity: 1 * 365, number_of_uploads: 2)
+passport = Tag.create(name: "Passeport", category: personnel, validity: 10 * 365, number_of_uploads: 1)
+
+driver_license = Tag.create(name: "Permis de conduire", category: auto, validity: 15 * 365, number_of_uploads: 2)
+gray_card = Tag.create(name: "Carte grise", category: auto, validity: 10 * 365, number_of_uploads: 1)
+insurance = Tag.create(name: "Attestation d'assurance", category: auto, validity: 10 * 365, number_of_uploads: 1)
+ct = Tag.create(name: "Controle technique", category: auto, validity: 2 * 365, number_of_uploads: 1)
+
+rib = Tag.create(name: "RIB", category: finance, validity: 10 * 365, number_of_uploads: 1)
+bank_statement = Tag.create(name: "Relevés bancaire", category: finance, validity: 5 * 365, number_of_uploads: 12)
+tax_notice = Tag.create(name: "Avis d'imposition", category: finance, validity: 3 * 365, number_of_uploads: 3)
+home_tax = Tag.create(name: "Taxe d'habitation", category: finance, validity: 1 * 365, number_of_uploads: 10)
+life_insurance = Tag.create(name: "Contrat d'assurance vie", category: finance, validity: 10 * 365, number_of_uploads: 10)
+mortgage = Tag.create(name: "Contrat de crédit immobilier", category: finance, validity: 2 * 365, number_of_uploads: 20)
+
+job = Tag.create(name: "Bulletins de salaire", category: emploi, validity: 1 * 365, number_of_uploads: 12)
+employment_contract = Tag.create(name: "Contrat de travail", category: emploi, validity: 10 * 365, number_of_uploads: 10)
+employment_notice = Tag.create(name: "Attestation d'embauche", category: emploi, validity: 10 * 365, number_of_uploads: 1)
+criminal_record = Tag.create(name: "Casier juficiaire", category: emploi, validity: 90, number_of_uploads: 1)
+
+health_book = Tag.create(name: "Carnet de santé", category: medical, validity: 10*365, number_of_uploads: 30)
+vital_card = Tag.create(name: "Carte vitale", category: medical, validity: 5*365, number_of_uploads: 1)
+proof_address = Tag.create(name: "Justificatif de domicile", category: habitation, validity: 60, number_of_uploads: 1)
+rent_receipt = Tag.create(name: "Quittance de loyer", category: habitation, validity: 10*365, number_of_uploads: 3)
+school_certificate = Tag.create(name: "Certificat de scolarité", category: education, validity: 10*365, number_of_uploads: 1)
+diplomas = Tag.create(name: "Diplomes", category: education, validity: 10*365, number_of_uploads: 5)
 
 puts "14 tags créés"
 puts "-------------"
