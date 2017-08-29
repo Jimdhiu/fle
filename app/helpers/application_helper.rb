adbe6c369325616d96de5ef8f8830dd6c38a71d0
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-29 16:23:27 +0200

    dernier commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,12 @@ module ApplicationHelper
 
   def check_documents_in_tag_expired?(documents, tag)
     expireds = []
-
-
     @documents.where(tag: tag).each do |document|
-
       if document.expired?
         expireds << true
       else
         expireds << false
       end
-
     end
     result = expireds.include? true
 
