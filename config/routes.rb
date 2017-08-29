a8e272657a9f33b1e0deeeb1330addd5d9460bff
jimmy dhiu <dhiu.jim@gmail.com>
2017-08-29 18:58:55 +0200

    Add page home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
   devise_for :users
-  root to: 'documents#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :documents, only: [:new, :create, :show, :edit, :update, :destroy ]
+  resources :documents, only: [:index, :new, :create, :show, :edit, :update, :destroy ]
 
 end
