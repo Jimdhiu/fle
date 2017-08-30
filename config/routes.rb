ecdfcf43fb8931b4f80c329368909920b841388d
jorge ruivinho <jorge.ruivinho@me.com>
2017-08-30 21:46:44 +0200

    status pending and status validated done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :documents, only: [:index, :new, :create, :show, :edit, :update, :destroy ]
 
-  resources :procedure_requests, only: [:index, :show, :destroy]
+  resources :procedure_requests, only: [:index, :update, :show, :destroy]
 
   namespace :pro do
     resources :procedure_requests
