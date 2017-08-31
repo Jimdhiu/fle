f8febbe732149aaa30d4b34eabf056368c3a89e6
Mathieu Lague <mathieu.lague@laposte.net>
2017-08-31 12:34:36 +0200

    ajout d'une route download_zip

--- app/controllers/pro/procedure_requests_controller.rb
+++ app/controllers/pro/procedure_requests_controller.rb
@@ -34,6 +34,45 @@ class Pro::ProcedureRequestsController < ApplicationController
   def destroy
   end
 
+  def download_zip
+    documents =[]
+    #récupérer les documents requis de la procedure en cours
+    self.procedure_tags.each do |tag|
+      self.part.documents.where(tag: tag).each do |document|
+        documents << document
+      end
+    end
+    byebug
+    #Attachment name
+    filename = 'documents.zip'
+    temp_file = Tempfile.new(filename)
+
+    begin
+      #This is the tricky part
+      #Initialize the temp file as a zip file
+      Zip::OutputStream.open(temp_file) { |zos| }
+
+      #Add files to the zip file as usual
+      Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
+        #Put files in here
+      end
+
+      #Read the binary data from the file
+      zip_data = File.read(temp_file.path)
+
+      #Send the data to the browser as an attachment
+      #We do not send the file directly because it will
+      #get deleted before rails actually starts sending it
+      send_data(zip_data, :type => 'application/zip', :filename => filename)
+    ensure
+      #Close and delete the temp file
+      temp_file.close
+      temp_file.unlink
+    end
+  end
+
+
+
   private
 
   def procedures_params
